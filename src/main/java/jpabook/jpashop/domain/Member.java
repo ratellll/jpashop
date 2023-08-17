eab5efb0f001be87789af46f28c7c722e43cdec4
ratellll <latellbin@naver.com>
2023-08-18 00:01:19 +0900

    feat: Money Embedded타입으로 생성
    Address를 참조하여 생성

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -27,6 +27,7 @@ public class Member {
 
     private int size;
 
+    @Embedded
     private Money money;
 
 }
--- src/main/java/jpabook/jpashop/domain/Money.java
+++ src/main/java/jpabook/jpashop/domain/Money.java
@@ -1,4 +1,16 @@
 package jpabook.jpashop.domain;
 
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Embeddable
+@Getter
+
 public class Money {
+
+    private int bankCount;
+
+
 }
