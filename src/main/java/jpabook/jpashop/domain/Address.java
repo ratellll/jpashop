63d4f4f34cf17ed5b1e4ea08e9f84654002a21a3
ratellll <latellbin@naver.com>
2023-08-23 23:17:36 +0900

    feat: Embeddable는 Getter만 생성후
    생성자 만들어주고 protected로 형식상 만들어준다
    
    값 타입은 변경 불가능하게 설계해야 한다.
    > @Setter 를 제거하고, 생성자에서 값을 모두 초기화해서 변경 불가능한 클래스를 만들자.
    JPA 스펙상 엔티티나 임베디드 타입( @Embeddable )은
     자바 기본 생성자(default constructor)를
     public 또는 protected 로 설정해야 한다.
     public 으로 두는 것 보다는 protected 로 설정하는 것이 그나마 더 안전 하다.

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -12,5 +12,14 @@ public class Address {
 
     private String city;
     private String street;
-    private String zipcode;
+   private String zipcode;
+
+
+   protected Address() { // JPA 스펙상 만들어 놓아야 한다.
+   }
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/Money.java
+++ src/main/java/jpabook/jpashop/domain/Money.java
@@ -12,5 +12,9 @@ public class Money {
 
     private int bankCount;
 
-
+    protected Money() {
+    }
+    public Money(int bankCount) {
+        this.bankCount = bankCount;
+    }
 }
