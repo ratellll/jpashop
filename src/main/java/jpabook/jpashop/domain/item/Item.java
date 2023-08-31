13bf4dfdf677d0f6195e5db69431fef52a8995b6
ratellll <latellbin@naver.com>
2023-08-31 23:29:43 +0900

    feat: 재고 비지니스로직 작성
    데이터를 가지고있는쪽에 비지니스 메서드를 만드는것이 응집력이있음
    setter를 사용하지않고 비지니스메서드를 이용해서 값을 변경하는게 최고

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,5 +27,33 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items") // 다대다 관계는 중간에 테이블을 하나 더 만들어서 관리한다.
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 데이터를 가지고 있는 쪽에 비즈니스 메서드를 만드는 것이 응집력이 있다.
+    // setter를 사용하지 않고 비즈니스 메서드를 사용해서 값을 변경하는 것이 좋다.
+
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+
+        this.stockQuantity = restStock;
+}
+
+
+
 
 }
