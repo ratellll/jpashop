f87e65be53b298870f443c03f35653dd1e74bf17
ratellll <latellbin@naver.com>
2024-07-15 13:15:05 +0900

    feat: account 및 Item 비즈니스 로직 추가 tip :

--- src/main/java/jpabook/jpashop/domain/Account.java
+++ src/main/java/jpabook/jpashop/domain/Account.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 
 import jakarta.persistence.*;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +16,34 @@ public class Account {
     @Column(name = "account_id")
     private Long id;
 
-    private Long point;
+    private int point;
 
-    private Long balance;
+    private int balance;
 
     @OneToOne(mappedBy = "account",fetch = FetchType.LAZY)
     private Member member;
+
+
+    //==비즈니스 로직==//
+
+    /**
+     * 잔고 증가
+     * @param addBalance
+     */
+    public void addBalance(int addBalance) {
+        this.balance += addBalance;
+    }
+
+
+    /**
+     * 잔고 감소
+     */
+    public void removeBalance(int totalPrice) {
+        int restBalance = this.balance - totalPrice;
+        if (restBalance < totalPrice) {
+            throw new NotEnoughStockException("need more Money");
+        }
+        this.balance = restBalance;
+    }
 
 }
--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,7 +1,67 @@
 package jpabook.jpashop.service;
 
 
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
 public class MemberServiceTest {
+
+        @Autowired
+        MemberService memberService;
+        @Autowired
+        MemberRepository memberRepository;
+
+    @Test
+    @Rollback(false)
+    public void 회원가입() throws Exception{
+        //given
+        Member member = new Member();
+        member.setName("빈이");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Assertions.assertEquals(member, memberRepository.findOne(saveId));
+
+     }
+
+@Test
+public void 중복회원_예외() throws Exception{
+    //given
+    Member member1 = new Member();
+    Member member2 = new Member();
+    member1.setName("빈");
+    member2.setName("빈");
+
+    //when
+    memberService.join(member1);
+    try {
+        memberService.join(member2);
+    }catch (IllegalStateException e){
+        return;
+    }
+
+    //then
+    fail("예외 발생 X");
+
+ }
 
 
 }
+
+
