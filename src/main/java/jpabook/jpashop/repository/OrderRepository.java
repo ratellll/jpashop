29281cc65cf36a4c18e1ae4d215164ef4462d35b
ratellll <latellbin@naver.com>
2024-07-16 17:24:52 +0900

    feat: OrderSerach 생성 but 상세히 확인다시해봐야함 tip :

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
@@ -20,5 +22,15 @@ public class OrderRepository {
 
     public Order findOne(Long id) {
         return em.find(Order.class, id);
+    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status =:status" +
+                        " and m.name like :name", Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name",orderSearch.getMemberName())
+                .setMaxResults(100)
+                .getResultList();
+
     }
 }
